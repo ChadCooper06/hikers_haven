14dd9f4658e753b3b8423d4ad8f7030043fd07b6
ChadCooper06 <chad.cooper@student.awesomeinc.org>
2022-04-21 19:42:18 +0000

    Created 1st model
    - Forum model is made and can be connected to/added to
    - Logged into DB as admin

--- hikers_haven/models.py
+++ hikers_haven/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Forum(models.Model):
+    id = models.IntegerField(primary_key=True)
+    topics = models.CharField(max_length=200, blank=False, null=False)
+        
+    def __str__(self):
+        return self.topics
